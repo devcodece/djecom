10fbe5fb84aee665f088b9b832702ff98a854009
devcodece <devcodece@gmail.com>
2020-10-07 21:48:22 -0600

    Se agregaron las funcionalidades para el calculo del total de productos y cantidad a travez del order-summary.html, respectivas funciones en views.py

--- djecom/urls.py
+++ djecom/urls.py
@@ -15,13 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from core.views import HomeView, ItemDetailView, add_to_cart, remove_from_cart
+from core.views import HomeView, ItemDetailView, add_to_cart, remove_from_cart, remove_single_item_from_cart, OrderSummaryView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('account/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', HomeView.as_view(), name = 'home'),
     path('product-page/<slug>/', ItemDetailView.as_view(), name = 'product-page'),
     path('add-to-cart/<slug>/', add_to_cart, name = 'add-to-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name = 'remove-from-cart'),
+    path('order-summary/', OrderSummaryView.as_view(), name = 'order-summary'),
+    path('remove-item-from-cart/<slug>/', remove_single_item_from_cart, name = 'remove-item-from-cart'),
 ]
